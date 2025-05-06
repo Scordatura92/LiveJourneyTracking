9d12dc59d7be3c92f65399f7625f84578d3976e1
Brayen <117528933+Scordatura92@users.noreply.github.com>
2025-05-06 09:05:16 +0700

    Initial Commit

---
+++ Challenge2/Challenge2App.swift
@@ -0,0 +1,32 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Brayen Fredgin Cahyadi on 06/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Challenge2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
