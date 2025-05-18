6c4d9d4529351f3dd42bd461d83e6c568247b1b5
Brayen <117528933+Scordatura92@users.noreply.github.com>
2025-05-18 23:49:59 +0700

    feat: code cleanup

--- Challenge2/JourneyView.swift
+++ Challenge2/JourneyView.swift
@@ -14,108 +14,6 @@ struct RouteProgressView: View {
     let startIndex: Int
     let endIndex: Int
     
-    //    @StateObject private var viewModel = RouteTrackerViewModel(route: route, startIndex: startIndex, endIndex: endIndex)
-    //
-    //    var body: some View {
-    //        //        VStack(spacing: 20) {
-    //        //            Text("Route Progress")
-    //        //                .font(.title)
-    //        //
-    //        //            ProgressView(value: Double(viewModel.currentBusStopIndex), total: Double(viewModel.totalBusStops))
-    //        //                .progressViewStyle(LinearProgressViewStyle())
-    //        //                .padding()
-    //
-    //        NavigationStack {
-    //            VStack(alignment: .leading, spacing: 5) {
-    //                Text("Live Journey Update")
-    //                    .font(.callout)
-    //
-    //                HStack (spacing: 5){
-    //                    var startingStop: String = "Start"
-    //                    var destinationStop: String = "Stop"
-    //
-    //                    Text(startingStop)
-    //                        .font(.headline)
-    //                        .italic(true)
-    //
-    //                    Image(systemName: "arrow.forward")
-    //
-    //                    Text(destinationStop)
-    //                        .font(.headline)
-    //                        .italic(true)
-    //
-    //                }
-    //
-    ////                Spacer()
-    //
-    //                HStack (alignment: .top){
-    //                    Text("Next Stop:")
-    //                        .font(.headline)
-    //
-    //                    ZStack {
-    //                        Rectangle()
-    //                            .fill(Color.orange)
-    //                            .frame(width: 250, height: 60)
-    //                            .cornerRadius(5)
-    //
-    //                        VStack {
-    //
-    //                            Text("Next Stop")
-    //                                .font(.title2)
-    //                                .bold(true)
-    //                                .foregroundStyle(Color.white)
-    //
-    //
-    //
-    //                            Text("X Stops Remaining to Destination")
-    //                                .font(.caption)
-    //                                .italic(true)
-    //                                .foregroundStyle(Color.white)
-    //                        }
-    //                    }
-    //                }.padding(.vertical, 10)
-    //
-    //                Divider()
-    //
-    //                ScrollView {
-    //                    HStack(alignment: .top){
-    //                        VStack (alignment: .leading, spacing: 0){
-    //                            ForEach(0..<viewModel.totalBusStops, id: \.self) { index in
-    //                                HStack (alignment: .center){
-    //                                    VStack (spacing: 0){
-    //                                        Circle()
-    //                                            .fill(index < viewModel.currentBusStopIndex ? Color.orange : Color.gray)
-    //                                            .overlay(
-    //                                                Group {
-    //                                                    if index < viewModel.currentBusStopIndex {
-    //                                                        Image(systemName: "checkmark")
-    //                                                            .foregroundColor(.white)
-    //                                                    }
-    //                                                }
-    //                                            )
-    //                                            .frame(width: 20, height: 20)
-    //                                        Rectangle()
-    //                                            .fill(index < viewModel.currentBusStopIndex ? Color.orange : Color.gray)
-    //                                            .frame(width: 3, height: 80)
-    //                                    }
-    //
-    //                                    ImageStack(images: [viewModel.getImage(index: index)])
-    //
-    //                                    Text(viewModel.getLocationName(index: index))
-    //                                    Spacer()
-    //                                }
-    //                            }
-    //                        }
-    //                    }
-    //
-    //                }.padding(.vertical, 10)
-    //            }.padding(.all, 20)
-    //
-    //        }
-    //    }
-    //
-    //}
-    
     @StateObject private var viewModel: RouteTrackerViewModel
     
     init(route: RouteModel, startIndex: Int, endIndex: Int) {
@@ -217,3 +115,107 @@ struct RouteProgressView: View {
 #Preview {
     RouteProgressView(route: route2a, startIndex: 2, endIndex: 8)
 }
+
+
+//    @StateObject private var viewModel = RouteTrackerViewModel(route: route, startIndex: startIndex, endIndex: endIndex)
+//
+//    var body: some View {
+//        //        VStack(spacing: 20) {
+//        //            Text("Route Progress")
+//        //                .font(.title)
+//        //
+//        //            ProgressView(value: Double(viewModel.currentBusStopIndex), total: Double(viewModel.totalBusStops))
+//        //                .progressViewStyle(LinearProgressViewStyle())
+//        //                .padding()
+//
+//        NavigationStack {
+//            VStack(alignment: .leading, spacing: 5) {
+//                Text("Live Journey Update")
+//                    .font(.callout)
+//
+//                HStack (spacing: 5){
+//                    var startingStop: String = "Start"
+//                    var destinationStop: String = "Stop"
+//
+//                    Text(startingStop)
+//                        .font(.headline)
+//                        .italic(true)
+//
+//                    Image(systemName: "arrow.forward")
+//
+//                    Text(destinationStop)
+//                        .font(.headline)
+//                        .italic(true)
+//
+//                }
+//
+////                Spacer()
+//
+//                HStack (alignment: .top){
+//                    Text("Next Stop:")
+//                        .font(.headline)
+//
+//                    ZStack {
+//                        Rectangle()
+//                            .fill(Color.orange)
+//                            .frame(width: 250, height: 60)
+//                            .cornerRadius(5)
+//
+//                        VStack {
+//
+//                            Text("Next Stop")
+//                                .font(.title2)
+//                                .bold(true)
+//                                .foregroundStyle(Color.white)
+//
+//
+//
+//                            Text("X Stops Remaining to Destination")
+//                                .font(.caption)
+//                                .italic(true)
+//                                .foregroundStyle(Color.white)
+//                        }
+//                    }
+//                }.padding(.vertical, 10)
+//
+//                Divider()
+//
+//                ScrollView {
+//                    HStack(alignment: .top){
+//                        VStack (alignment: .leading, spacing: 0){
+//                            ForEach(0..<viewModel.totalBusStops, id: \.self) { index in
+//                                HStack (alignment: .center){
+//                                    VStack (spacing: 0){
+//                                        Circle()
+//                                            .fill(index < viewModel.currentBusStopIndex ? Color.orange : Color.gray)
+//                                            .overlay(
+//                                                Group {
+//                                                    if index < viewModel.currentBusStopIndex {
+//                                                        Image(systemName: "checkmark")
+//                                                            .foregroundColor(.white)
+//                                                    }
+//                                                }
+//                                            )
+//                                            .frame(width: 20, height: 20)
+//                                        Rectangle()
+//                                            .fill(index < viewModel.currentBusStopIndex ? Color.orange : Color.gray)
+//                                            .frame(width: 3, height: 80)
+//                                    }
+//
+//                                    ImageStack(images: [viewModel.getImage(index: index)])
+//
+//                                    Text(viewModel.getLocationName(index: index))
+//                                    Spacer()
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                }.padding(.vertical, 10)
+//            }.padding(.all, 20)
+//
+//        }
+//    }
+//
+//}
+
